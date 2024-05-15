b3998949c1a0171bcf7991c003c232906a3d32c7
fredo09 <fred_15@outlook.com>
2024-05-14 20:58:25 -0600

    add file config  prisma client a next.js

---
+++ src/lib/prisma.ts
@@ -0,0 +1,18 @@
+/**
+ * Configuracion de prisma client en next
+ */
+
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  prisma = (global as any).prisma;
+}
+
+export default prisma;
