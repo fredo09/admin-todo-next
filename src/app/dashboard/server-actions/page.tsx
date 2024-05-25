9e6dadeb689b6aaa96e494976456687effd387b5
fredo09 <fred_15@outlook.com>
2024-05-24 21:48:03 -0600

    add new page server actions

---
+++ src/app/dashboard/server-actions/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: "server Actions",
+    description: "aprender server actions y use Optimistic de react"
+}
+
+export default async function ServerActionsPage() {
+    return (
+        <div>
+            <h1>Server actions 🚀 </h1>
+        </div>
+    );
+}
