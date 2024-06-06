1a86b1caafbff5030deca78fd8e6f57d92b25c83
fredo09 <fred_15@outlook.com>
2024-06-06 00:11:33 -0600

    add total to pay for shopping card

--- src/app/dashboard/cart/page.tsx
+++ src/app/dashboard/cart/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { Product, products } from "@/products/data";
 import { cookies } from "next/headers";
 import { ItemCard } from "@/shopping-cart/components";
+import { Widget } from "@/components";
 
 export const metadata:Metadata = {
     title: "Carrito de compras",
@@ -32,14 +33,18 @@ export default async function CartPage () {
     const cartCokkie = JSON.parse( cookieStore.get('cart')?.value ?? '{}' ) as { [id: string]: number };
 
     const itemsInCart = getProductsCart(cartCokkie);
-    console.log("🚀 ~ CartPage ~ itemsInCart:", itemsInCart)
+    console.log("🚀 ~ CartPage ~ itemsInCart:", itemsInCart);
+
+    //Total items cart
+    const totalToPay = itemsInCart.reduce(  (prev, current) => (current.product.price * current.quantity) + prev , 0)
+    console.log("🚀 ~ CartPage ~ totalToPay:", totalToPay);
 
     return (
         <div className="bg-white p-4 rounded">
             <h1 className="text-5xl"> Productos en el carrito </h1>
             <hr className="mb-2"></hr>
 
-            <div className="flex flex-cols sm:flex-row gap-2 w-full">
+            <div className="flex flex-col sm:flex-row gap-2 w-full">
                 <div className="flex flex-col gap-2 w-full sm:w-8/12">
                     {
                         itemsInCart.map(({ product, quantity }) => (
@@ -50,6 +55,14 @@ export default async function CartPage () {
                             />
                         ))
                     }
+                </div>
+                <div className="flex flex-col w-full sm:w-4/12">
+                    <Widget title="Total a pagar">
+                        <div className="mt-2 flex justify-center gap-4">
+                            <h3 className="text-3xl font-bold text-gray-700">$ {(totalToPay * 1.15).toFixed(2)}</h3>
+                        </div>
+                        <span className="font-bold text-center ">Impuesto 15%: ${(totalToPay * 0.15).toFixed(2)} </span>
+                    </Widget>
                 </div>
             </div>
         </div>
