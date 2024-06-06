403e53ba69ad73a3d20957d9a1d9f05e23acb39e
fredo09 <fred_15@outlook.com>
2024-06-05 20:07:37 -0600

    modify topNavBar for shopping-cart

--- src/components/TopNavbar/TopNavbar.tsx
+++ src/components/TopNavbar/TopNavbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CiBellOn, CiChat1, CiMenuBurger, CiSearch } from 'react-icons/ci';
+import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci';
 
 export const TopNavbar = () => {
   return (
@@ -27,8 +27,10 @@ export const TopNavbar = () => {
               <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
                 <CiChat1 size={25} />
               </button>
-              <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-                <CiBellOn size={25}/>
+              <button className="flex items-center justify-center p-2 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+                {/* <CiBellOn size={25}/> */}
+                <span className='text-sm mr-2 text-blue-800 font-bold'> 10 </span>
+                <CiShoppingBasket size={25}/>
               </button>
             </div>
           </div>
