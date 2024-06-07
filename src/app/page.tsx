0c578863e8dae4c12c14d4fdaee192505f815681
fredo09 <fred_15@outlook.com>
2024-06-06 20:22:23 -0600

    Refactor page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import { redirect } from "next/navigation";
 
 export default function HomePage() {
 
-  redirect('dashboard/rest-todo');
+  redirect('/dashboard');
 
   // return (
-  //   <div>
+  //   <div className="grid gap-6 grid-cols-1 sm:grid-cols-2">
   //     <h1 className="text-4xl">HOLA DAHSBOARD 🚀 </h1>
   //   </div>
   // );
