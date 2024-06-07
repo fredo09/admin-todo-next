b57efdab04ccf3133c70cde9098db868f515bd21
fredo09 <fred_15@outlook.com>
2024-06-07 00:15:38 -0600

    add signin and signOut auth.js

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { CiLogout, CiBookmarkCheck, CiUser } from 'react-icons/ci';
-import { SidebarItem } from '../SidebarItem/SidebarItem';
+import { CiBookmarkCheck, CiUser } from 'react-icons/ci';
+import { SidebarItem, Logout } from '@/components';
 
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
@@ -95,12 +95,7 @@ export const Sidebar = async () => {
         </ul>
       </div>
 
-      <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-          <CiLogout />
-          <span className="group-hover:text-gray-700">Logout</span>
-        </button>
-      </div>
+      <Logout />
     </aside>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@
 
 export { Sidebar } from './Sidebar/Sidebar';
 export { SidebarItem } from './SidebarItem/SidebarItem';
+export { Logout } from './Logout/Logout';
 export { TopNavbar } from './TopNavbar/TopNavbar';
 export { TabBar } from './TabBar/TabBar';
 export { Widget } from './Widget/Widget'
