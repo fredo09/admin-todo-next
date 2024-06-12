509c1a48b0b73f3430c0dd01fd8a4065000b2321
fredo09 <fred_15@outlook.com>
2024-06-12 01:52:07 -0600

    add config about jwt and session user logged

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -21,7 +21,42 @@ export const authOptions:NextAuthOptions = {
             clientId: process.env.AUTH_GITHUB_ID ?? '',
             clientSecret: process.env.AUTH_GITHUB_SECRET ?? ''
         }),
-    ]
+    ],
+    //* Algunas configurariones extras acerca de la sesion
+    session: {
+        strategy: 'jwt' // * -> configurarion con jwt
+    },
+    callbacks:{ 
+        //* funciones que se ejecutan despues de hacer la sesion mediante los providers
+        async signIn({ user, account, profile, email, credentials }) {
+            // * ... agrega nueva configuracion
+            return true;
+        },
+        async jwt({ token, user, account, profile }){
+            // * ... agrega nueva configuracion
+            const dbUser = await prisma.user.findUnique({ where: { email: token.email ?? 'no-email' } });
+
+            //* preguntamos si el usuario no esta activo
+            if ( dbUser?.isActive === false){
+                throw Error ('El usuario no esta activo');
+            }
+
+            token.roles = dbUser?.roles ?? ['no-role'];
+            token.id = dbUser?.id ?? 'no-id'
+
+            return token;
+        },
+        async session({ session, token, user }) {
+            // * ... agrega nueva configuracion
+            console.log({ token })
+
+            if ( session && session.user){
+                session.user.roles = token.roles;
+                session.user.id = token.id;
+            }
+            return session;
+        }
+    }
 };
 
 const handler = NextAuth(authOptions);
